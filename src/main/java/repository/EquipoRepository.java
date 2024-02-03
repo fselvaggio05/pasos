80f1b81b7e22b388ac47c139be46826586e71da7
andreabgurdulich <andreabgurdulich@gmail.com>
2024-02-03 13:11:50 -0300

    Update Classpath

--- src/main/java/repository/EquipoRepository.java
+++ src/main/java/repository/EquipoRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package repository;
 
 import java.sql.Date;
@@ -231,65 +230,3 @@ public class EquipoRepository {
 		return respuestaOperacion;
 	}
 }
-=======
-package repository;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.mysql.cj.xdevapi.PreparableStatement;
-
-import conexionDB.FactoryConnection;
-import entity.Equipo;
-
-public class EquipoRepository {
-	
-	ResultSet rs = null;
-	PreparedStatement stmt = null;
-
-	public List<Equipo> getAll() {
-		
-		List<Equipo> equipos = new ArrayList<>();
-		
-		try {
-			
-			
-			stmt = FactoryConnection.getInstancia().getConn().prepareStatement("select id_equipo, descripcion from equipo where estado=1");
-			rs = stmt.executeQuery();
-			while (rs!=null && rs.next())
-			{
-				Equipo eq = new Equipo();
-				eq.setId_equipo(rs.getInt("id_equipo"));
-				eq.setDescripcion(rs.getString("descripcion"));				
-				equipos.add(eq);
-			}
-			
-			
-		} 
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		finally {
-			
-			 try {
-		            if (rs != null) rs.close();
-		            if (stmt != null) stmt.close();
-		          } 
-		        catch (Exception e) {
-		            e.printStackTrace();
-		        }
-
-		        FactoryConnection.getInstancia().releaseConn();
-			
-		}
-		
-		return equipos;
-	}
-
-}
->>>>>>> a7c1ad3889e99405d1dc4eb65dabd004bf1816e3
